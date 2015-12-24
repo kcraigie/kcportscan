dac26f07a31ad6be01dc248398095f7937932b96
Keith Craigie <info@kcraigie.com>
2015-12-24 01:29:29 -0800

    Added support for block of addresses by prefix

--- kcportscan.cpp
+++ kcportscan.cpp
@@ -10,31 +10,22 @@
 #include <sys/time.h>
 
 typedef unsigned char uint8_t;
+typedef unsigned int uint_t;
 
 struct SocketInfo_t {
   int socket;
-  // int address;
+  uint_t address;
   int port;
   int isValid;
   unsigned long long startTicks;
 };
 
 static unsigned long long g_timeout = 3000ULL;
-
 static int g_maxSockets = 1024;
-static int g_numSockets = 0;
-static SocketInfo_t * g_sinfos = NULL;
-
-static pollfd * g_pollfds = NULL;
-static int * g_pollfdsToSocketIndex = NULL;
-static int g_numpollfds = 0;
-
-static int g_nextPort = 1;
-static unsigned int g_address = 0;
 
 static void printUsage(char * argv0)
 {
-  fprintf(stderr, "Usage: %s -4 [ipv4_address] -d <timeout>\n", argv0);
+  fprintf(stderr, "Usage: %s -4 [ipv4_address] -t <timeout_ms>\n", argv0);
 }
 
 static unsigned long long getticks()
@@ -46,80 +37,25 @@ static unsigned long long getticks()
   return (unsigned long long)tv.tv_sec + (unsigned long long)tv.tv_usec / 1000ULL;
 }
 
-int main(int argc, char * argv[])
+static void portScanAddress(uint_t address)
 {
-  int needNewline = 0;
+  SocketInfo_t * sinfos = (SocketInfo_t*)malloc(sizeof(SocketInfo_t) * g_maxSockets);
+  pollfd * pollfds = (pollfd*)malloc(sizeof(pollfd) * g_maxSockets);
+  int * pollfdsToSocketIndex = (int*)malloc(sizeof(int) * g_maxSockets);
 
-  for(int i=1;i<argc;i++) {
-    switch(argv[i][0]) {
-    case '-':
-      switch(argv[i][1]) {
-      case '4':
-        {
-          if(i+1>argc) {
-            printUsage(argv[0]);
-            return 1;
-          }
-          int i0, i1, i2, i3, iMask;
-          if(sscanf(argv[i+1], "%d.%d.%d.%d/%d", &i0, &i1, &i2, &i3, &iMask)==5) {
-            // User specified address including mask
-            g_address = (unsigned int)i0 << 24 & 0xff000000;
-            g_address |= (unsigned int)i1 << 16 & 0x00ff0000;
-            g_address |= (unsigned int)i2 << 8 & 0x0000ff00;
-            g_address |= (unsigned int)i3 << 0 & 0x000000ff;
-            // mask = iMask;
-          } else if(sscanf(argv[i+1], "%d.%d.%d.%d", &i0, &i1, &i2, &i3)==4) {
-            // User specified address without mask
-            g_address = (unsigned int)i0 << 24 & 0xff000000;
-            g_address |= (unsigned int)i1 << 16 & 0x00ff0000;
-            g_address |= (unsigned int)i2 << 8 & 0x0000ff00;
-            g_address |= (unsigned int)i3 << 0 & 0x000000ff;
-          }
-        }
-        break;
-      case 't':
-        {
-          if(i+1>argc) {
-            printUsage(argv[0]);
-            return 1;
-          }
-          int i0;
-          if(sscanf(argv[i+1], "%d", &i0)==1) {
-            // User specified timeout
-            g_timeout = (unsigned long long)i0 * 1000ULL;
-          }
-        }
-        break;
-      }
-    }
-  }
-
-  if(g_address==0) {
-    printUsage(argv[0]);
-    return 1;
-  }
-
-  fprintf(stderr, "Address block to portscan: %d.%d.%d.%d/%d\n",
-          g_address >> 24 & 0xff, g_address >> 16 & 0xff, g_address >> 8 & 0xff, g_address & 0xff, 32);
-
-  struct rlimit rlim;
-  if(getrlimit(RLIMIT_NOFILE, &rlim) == 0) {
-    g_maxSockets = rlim.rlim_cur;
-  }
-  fprintf(stderr, "Maximum number of file descriptors: %d\n", g_maxSockets);
-  g_sinfos = (SocketInfo_t*)malloc(sizeof(SocketInfo_t) * g_maxSockets);
-  g_pollfds = (pollfd*)malloc(sizeof(pollfd) * g_maxSockets);
-  g_pollfdsToSocketIndex = (int*)malloc(sizeof(int) * g_maxSockets);
+  int numSockets = 0;
+  int numpollfds = 0;
+  int nextPort = 1;
 
   // Main loop
   while(1) {
     int j, ret;
 
-    g_numpollfds = 0;
+    numpollfds = 0;
 
     // Fill up and compact socket array while refilling pollfd array
-    for(j=0;j<g_maxSockets && g_nextPort<=65535;j++) {
-      if(!g_sinfos[j].isValid) {
+    for(j=0;j<g_maxSockets && nextPort<=65535;j++) {
+      if(!sinfos[j].isValid) {
         ret = -1;
 
         int s = socket(AF_INET, SOCK_STREAM, 0);
@@ -133,94 +69,95 @@ int main(int argc, char * argv[])
             // Set up address and port
             sockaddr_in sa;
             sa.sin_family = AF_INET;
-            sa.sin_addr.s_addr = htonl(g_address);
-            sa.sin_port = htons(g_nextPort);
+            sa.sin_addr.s_addr = htonl(address);
+            sa.sin_port = htons(nextPort);
 
             ret = connect(s, (sockaddr*)&sa, sizeof(sa));
             if(ret==-1) {
               if(errno == EINPROGRESS) {
                 ret = 0;
               } else {
-                fprintf(stderr, "Unknown error connecting to port %d: %d\n", g_nextPort, errno);
+                fprintf(stderr, "\rUnknown error connecting to %d.%d.%d.%d:%d: %d\n",
+                        address >> 24 & 0xff, address >> 16 & 0xff, address >> 8 & 0xff, address & 0xff,
+                        nextPort, errno);
               }
             } else if(ret==0) {
-              printf("%sPort %d accepted IPv4 TCP connection immediately\n", (needNewline?"\n":""), ntohs(sa.sin_port));
-              needNewline = 0;
+              printf("%d.%d.%d.%d:%d accepted IPv4 TCP connection immediately\n",
+                     address >> 24 & 0xff, address >> 16 & 0xff, address >> 8 & 0xff, address & 0xff,
+                     ntohs(sa.sin_port));
               // Just clean it up like an error
               ret = -1;
             }
 
           } else {
-            fprintf(stderr, "Failed to make socket non-blocking: %d\n", errno);
+            fprintf(stderr, "\rFailed to make socket non-blocking: %d\n", errno);
           }
 
           // Save socket or clean up
           if(ret!=-1) {
-            g_sinfos[j].socket = s;
-            // g_sinfos[j].address = g_address;
-            g_sinfos[j].port = g_nextPort;
-            g_sinfos[j].isValid = 1;
-            g_sinfos[j].startTicks = getticks();
-            g_numSockets++;
+            sinfos[j].socket = s;
+            sinfos[j].address = address;
+            sinfos[j].port = nextPort;
+            sinfos[j].isValid = 1;
+            sinfos[j].startTicks = getticks();
+            numSockets++;
           } else {
             close(s);
             s = -1;
           }
 
-          g_nextPort++;
+          nextPort++;
         } else if(errno == EMFILE) {
           // Out of file descriptors
           break;
         } else {
-          fprintf(stderr, "Unknown error creating socket: %d\n", errno);
+          fprintf(stderr, "\rUnknown error creating socket: %d\n", errno);
         }
-      } // if(!g_sinfos[j].isValid) {
+      } // if(!sinfos[j].isValid) {
 
-      if(g_sinfos[j].isValid) {
-        g_pollfds[g_numpollfds].fd = g_sinfos[j].socket;
-        g_pollfds[g_numpollfds].events = POLLOUT;
-        g_pollfds[g_numpollfds].revents = 0;
-        g_pollfdsToSocketIndex[g_numpollfds] = j;
-        g_numpollfds++;
+      if(sinfos[j].isValid) {
+        pollfds[numpollfds].fd = sinfos[j].socket;
+        pollfds[numpollfds].events = POLLOUT;
+        pollfds[numpollfds].revents = 0;
+        pollfdsToSocketIndex[numpollfds] = j;
+        numpollfds++;
       }
 
     } // for(;j<g_maxSockets;j++) {
 
-    if(g_numpollfds==0) {
+    if(numpollfds==0) {
       break;
     }
 
-    ret = poll(g_pollfds, g_numpollfds, g_timeout);
+    ret = poll(pollfds, numpollfds, g_timeout);
 
     if(ret == -1) {
-      fprintf(stderr, "Unknown error polling %d fds: %d\n", g_numpollfds, errno);
-    } else if(ret>0) {
+      fprintf(stderr, "\rUnknown error polling %d fds: %d\n", numpollfds, errno);
+    } else {
       unsigned long long nowTicks = getticks();
-      for(j=0;j<g_numpollfds;j++) {
-        SocketInfo_t * sinfo = &g_sinfos[g_pollfdsToSocketIndex[j]];
+      for(j=0;j<numpollfds;j++) {
+        SocketInfo_t * sinfo = &sinfos[pollfdsToSocketIndex[j]];
         int shouldCloseSocket = 0;
-        if(g_pollfds[j].revents & POLLOUT) {
+        if(pollfds[j].revents & POLLOUT) {
           int err;
           socklen_t errlen = sizeof(err);
           if(getsockopt(sinfo->socket, SOL_SOCKET, SO_ERROR, &err, &errlen)!=-1) {
             if(err==0) {
-              printf("%sPort %d accepted IPv4 TCP connection\n", (needNewline?"\n":""), sinfo->port);
-              needNewline = 0;
-              if(sinfo->port>10000) {
-                int k = 0;
-              }
+              printf("%d.%d.%d.%d:%d accepted IPv4 TCP connection\n",
+                     address >> 24 & 0xff, address >> 16 & 0xff, address >> 8 & 0xff, address & 0xff,
+                     sinfo->port);
             } else {
               // printf("\rPort %d rejected IPv4 TCP connection: %d\n", sinfo->port, err);
             }
           } else {
-            fprintf(stderr, "Unknown error getting connect status: %d\n", errno);
+            fprintf(stderr, "\rUnknown error getting connect status: %d\n", errno);
           }
           shouldCloseSocket = 1;
-        } else if(g_pollfds[j].revents & POLLERR) {
+        } else if(pollfds[j].revents & POLLERR) {
           // printf("Port %d error on IPv4 TCP connection\n", sinfo->port);
           shouldCloseSocket = 1;
-        } else if(g_pollfds[j].revents) {
-          fprintf(stderr, "Unknown poll events received 0x%x\n", g_pollfds[j].revents);
+        } else if(pollfds[j].revents) {
+          fprintf(stderr, "\rUnknown poll events received 0x%x\n", pollfds[j].revents);
           shouldCloseSocket = 1;
         } else if(sinfo->startTicks - nowTicks > g_timeout) {
           // printf("Port %d timed out on IPv4 TCP connection\n", sinfo->port);
@@ -233,13 +170,90 @@ int main(int argc, char * argv[])
       }
     }
 
-    printf("...%d", g_nextPort);
-    fflush(stdout);
-    needNewline = 1;
+    fprintf(stderr, "...%d", nextPort);
+    fflush(stderr);
 
   } // while(1) {
 
-  if(needNewline) {
-    printf("\n");
+  free(sinfos);
+  free(pollfds);
+  free(pollfdsToSocketIndex);
+}
+
+static uint_t bitmaskifyPrefix(uint_t iPrefix)
+{
+  uint_t ret = 0;
+  for(uint_t i=0;i<iPrefix;i++) {
+    ret |= (uint_t)1 << (32U-i-1);
+  }
+  return ret;
+}
+
+int main(int argc, char * argv[])
+{
+  uint_t address = 0;
+  uint_t prefix = 0;
+
+  // Parse command-line arguments
+  for(int i=1;i<argc;i++) {
+    switch(argv[i][0]) {
+    case '-':
+      switch(argv[i][1]) {
+      case '4':
+        {
+          if(i+1>argc) {
+            printUsage(argv[0]);
+            return 1;
+          }
+          int i0, i1, i2, i3, i4 = 32;
+          if(sscanf(argv[i+1], "%d.%d.%d.%d/%d", &i0, &i1, &i2, &i3, &i4)==5 ||
+             sscanf(argv[i+1], "%d.%d.%d.%d", &i0, &i1, &i2, &i3)==4) {
+            address = (uint_t)i0 << 24 & 0xff000000;
+            address |= (uint_t)i1 << 16 & 0x00ff0000;
+            address |= (uint_t)i2 << 8 & 0x0000ff00;
+            address |= (uint_t)i3 << 0 & 0x000000ff;
+            prefix = i4;
+          }
+        }
+        break;
+      case 't':
+        {
+          if(i+1>argc) {
+            printUsage(argv[0]);
+            return 1;
+          }
+          int i0;
+          if(sscanf(argv[i+1], "%d", &i0)==1) {
+            // User specified timeout
+            g_timeout = (unsigned long long)i0;
+          }
+        }
+        break;
+      }
+    }
+  }
+
+  // Validate address block
+  if(address==0 || (prefix < 1 || prefix > 32)) {
+    printUsage(argv[0]);
+    return 1;
+  }
+
+  struct rlimit rlim;
+  if(getrlimit(RLIMIT_NOFILE, &rlim) == 0) {
+    g_maxSockets = rlim.rlim_cur;
+  }
+  fprintf(stderr, "\rMaximum number of file descriptors: %d\n", g_maxSockets);
+
+  uint_t prefixBitmask = bitmaskifyPrefix(prefix);
+  fprintf(stderr, "\rAddress block to portscan: %d.%d.%d.%d/%d\n",
+          (address & prefixBitmask) >> 24 & 0xff, (address & prefixBitmask) >> 16 & 0xff,
+          (address & prefixBitmask) >> 8 & 0xff, (address & prefixBitmask) & 0xff, prefix);
+  for(uint_t address2 = address & prefixBitmask;               // Start with first address in range
+      (address & prefixBitmask) == (address2 & prefixBitmask); // Continue while address in range
+      address2++) {                                            // Increment to next address
+    fprintf(stderr, "\rPortscanning: %d.%d.%d.%d\n",
+            address2 >> 24 & 0xff, address2 >> 16 & 0xff, address2 >> 8 & 0xff, address2 & 0xff);
+    portScanAddress(address2);
   }
 }
