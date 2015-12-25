ba684ad838ed029c5dae144e5d318207ac679e2a
Keith Craigie <info@kcraigie.com>
2015-12-25 02:22:30 -0800

    Cleaned up test, kills any remaining listeners when kcportscan finishes

--- test_kcportscan.sh
+++ test_kcportscan.sh
@@ -9,14 +9,14 @@ for i in `eval echo "{0..$NUM_LISTENERS}"`; do
     ports[$i]="$((((RANDOM + RANDOM) % 63001) + 2000))"
 done
 
-echo -n "Spawning `expr $NUM_LISTENERS + 1` random listeners on $LISTEN_HOST"
+echo -n "Listening on `expr $NUM_LISTENERS + 1` random ports on $LISTEN_HOST"
 for port in ${ports[@]}; do
+    echo -n "...$port"
     ./kclisten $LISTEN_HOST:$port 1>/dev/null &
-    echo -n "."
 done
 echo
 
-echo "Spawning kcportscan on $LISTEN_HOST/$SCAN_BLOCK_PREFIX..."
+echo "Starting kcportscan on $LISTEN_HOST/$SCAN_BLOCK_PREFIX..."
 ./kcportscan -4 $LISTEN_HOST/$SCAN_BLOCK_PREFIX -t $SCAN_TIMEOUT >&/tmp/kcportscan.log &
 job_id=$!
 
@@ -24,17 +24,20 @@ echo -n "Waiting for kcportscan ($job_id) to finish..."
 wait $job_id
 echo
 
-echo -n "See whether kcportscan found all of the random ports"
+# Kill any remaining listeners
+for job_id in `jobs -p`; do
+    kill $job_id && wait $job_id
+done
+
+echo -n "Determining whether kcportscan found all the random ports"
 for port in ${ports[@]}; do
     grep -q "$LISTEN_HOST:$port accepted " /tmp/kcportscan.log
     if [[ $? != 0 ]]; then
-        echo "FAILURE: kcportscan missed finding $LISTEN_HOST:$port!"
+        echo "...FAILURE! Missed $LISTEN_HOST:$port"
         exit 1
     fi
-    echo -n "."
+    echo -n "...$port"
 done
-echo "SUCCESS!"
+echo "...SUCCESS!"
 
 exit 0
-
-# killall kclisten
